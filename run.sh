78617ddf7f6bfb12b658d83fb171422c21844f91
Ainuddin Faizan <andyfaizan@gmail.com>
2017-03-05 23:26:49 +0100

    Pushed docker images to dockerhub; removed build commands

--- run.sh
+++ run.sh
@@ -10,13 +10,13 @@ fi
 cd 'mytomcatdocker'
 
 if [ "$env" != "prod" ]; then
-    docker build -f Dockerfile-dev -t andy/tomcat-dev .
-    docker run --rm --name tomcat-dev -p 8080:8080 andy/tomcat-dev &
+    docker pull andyfaizan/tomcat-dev # image on docker hub; building not required
+    docker run --rm --name tomcat-dev -p 8080:8080 andyfaizan/tomcat-dev &
 else
     ### Scenario 2 ###
     # Running this docker container with another container for spotlight on the same host
-    docker build -f Dockerfile-prod -t andy/tomcat-prod .
-    docker run --rm --link spotlight:english_spotlight --name tomcat -p 8080:8080 andy/tomcat-prod &
+    docker pull andyfaizan/tomcat-prod
+    docker run --rm --link spotlight:english_spotlight --name tomcat -p 8080:8080 andyfaizan/tomcat-prod &
 fi
 
 cd '..'
