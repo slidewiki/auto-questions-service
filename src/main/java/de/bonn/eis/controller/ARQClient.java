7ed01ee33d5765586b40dcff4246b7daf5f190d4
Ainuddin Faizan <andyfaizan@gmail.com>
2017-03-19 18:37:35 +0100

    Refactor code of getting specific types

--- src/main/java/de/bonn/eis/controller/ARQClient.java
+++ src/main/java/de/bonn/eis/controller/ARQClient.java
@@ -1,5 +1,8 @@
 package de.bonn.eis.controller;
 
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multimaps;
 import de.bonn.eis.model.DBPediaResource;
 import de.bonn.eis.utils.NLPConsts;
 import de.bonn.eis.utils.QGenLogger;
@@ -13,7 +16,9 @@ import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Created by Ainuddin Faizan on 1/7/17.
@@ -26,9 +31,10 @@ public class ARQClient {
     private static final String PREFIX_FOAF = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n";
     private static final String PREFIX_RDFS = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n";
     private static final String PREFIX_WIKIDATA = "PREFIX wikidata: <http://www.wikidata.org/entity/>\n";
-    private static final String TIMEOUT_VALUE = "5000";
+    private static final String TIMEOUT_VALUE = "10000";
     private static final String UNION = "UNION\n";
     private static final int ALLOWED_DEPTH_FOR_MEDIUM = 8;
+    private static final String DBPEDIA = "dbpedia";
     private final String PREFIX_DBRES = "PREFIX dbres: <http://dbpedia.org/ontology/>\n";
     private final String PREFIX_SCHEMA = "PREFIX schema: <http://schema.org/>\n";
     private final String PREFIX_DUL = "PREFIX dul: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl>\n";
@@ -39,10 +45,10 @@ public class ARQClient {
         List<String> resourceNames = new ArrayList<>();
         List<String> resourceTypeList = null;
 
-        if(level.equals(NLPConsts.LEVEL_EASY)){
+        if (level.equals(NLPConsts.LEVEL_EASY)) {
             String[] types = resource.getTypes().split(",");
             resourceTypeList = Arrays.asList(types[types.length - 1]);
-        } else if(level.equals(NLPConsts.LEVEL_MEDIUM) || level.equals(NLPConsts.LEVEL_HARD)){
+        } else if (level.equals(NLPConsts.LEVEL_MEDIUM) || level.equals(NLPConsts.LEVEL_HARD)) {
             resourceTypeList = getResourceTypes(resource);
             if (resourceTypeList != null && !resourceTypeList.isEmpty()) {
                 if (resourceTypeList.size() == 1) {
@@ -53,7 +59,7 @@ public class ARQClient {
                 }
                 QGenLogger.info("Resource: " + resource.getSurfaceForm());
                 int maxAllowedDepth = Integer.MAX_VALUE;
-                if(level.equals(NLPConsts.LEVEL_MEDIUM)){
+                if (level.equals(NLPConsts.LEVEL_MEDIUM)) {
                     maxAllowedDepth = ALLOWED_DEPTH_FOR_MEDIUM;
                 }
                 resourceTypeList = getMostSpecificTypes(resourceTypeList, maxAllowedDepth);
@@ -142,7 +148,7 @@ public class ARQClient {
     }
 
     private int getGroupSize(int sizeOfList) {
-        int maxGroupSize = 2;
+        int maxGroupSize = 3;
         if (sizeOfList <= maxGroupSize) {
             return sizeOfList - 1;
         }
@@ -191,8 +197,6 @@ public class ARQClient {
         return set;
     }
 
-    //TODO Select multiple types for more results or changing difficulty
-
     /**
      * Get the most specific rdf:type i.e. the one that has the most number of super classes
      *
@@ -209,46 +213,48 @@ public class ARQClient {
         }
 
         List<String> mostSpecificTypes = new ArrayList<>();
-        int maxPathDepth = 0;
+        Function<String, Integer> typeDepthFunction = this::getTypeDepth;
 
-        for (String type : types) {
-            if (type.toLowerCase().contains("dbpedia")) {
-//                if (type.toLowerCase().contains("yago") && !type.toLowerCase().contains("wikicat")) {
-//                    continue;
-//                }
-                    String queryString =
-                        PREFIX_RDF + PREFIX_FOAF + PREFIX_RDFS +
-                                "SELECT DISTINCT ?path FROM <http://dbpedia.org> WHERE {\n" +
-                                "<" + type + "> rdfs:subClassOf* ?path . }";
-
-                ResultSet results;
-                int count = 0;
-                try {
-                    results = runSelectQuery(queryString);
-                    while (results.hasNext()) {
-                        results.next();
-                        count++;
-                    }
-                    if (count <= maxAllowedDepth) {
-                        if (count > maxPathDepth) {
-                            if (!mostSpecificTypes.isEmpty()) {
-                                mostSpecificTypes.clear();
-                            }
-                            maxPathDepth = count;
-                            mostSpecificTypes.add(type);
-                        } else if (count == maxPathDepth) {
-                            mostSpecificTypes.add(type);
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+        ImmutableListMultimap<Integer, String> map = Multimaps.index(types, typeDepthFunction::apply);
+        ImmutableSet<Integer> keySet = map.keySet();
+        Integer maxDepth = Collections.max(keySet);
+        if (maxAllowedDepth != Integer.MAX_VALUE) {
+            maxDepth = maxAllowedDepth;
         }
-        if (mostSpecificTypes.size() > 5) {
-            mostSpecificTypes = mostSpecificTypes.subList(0, 6);
+        while (mostSpecificTypes.size() < 10) {
+            mostSpecificTypes.addAll(map.get(maxDepth));
+            maxDepth--;
+        }
+
+        // If env is dev
+        for (Integer key : keySet) {
+            System.out.println(key);
+            System.out.println(map.get(key));
         }
         return mostSpecificTypes;
+    }
+
+    private int getTypeDepth(String type) {
+        if (!type.toLowerCase().contains(DBPEDIA)) {
+            return 0;
+        }
+        String queryString =
+                PREFIX_RDF + PREFIX_FOAF + PREFIX_RDFS +
+                        "SELECT DISTINCT ?path FROM <http://dbpedia.org> WHERE {\n" +
+                        "<" + type + "> rdfs:subClassOf* ?path . }";
+
+        ResultSet results;
+        int count = 0;
+        try {
+            results = runSelectQuery(queryString);
+            while (results.hasNext()) {
+                results.next();
+                count++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return count;
     }
 
     private String addPrefix(String queryString, String namespace) {
